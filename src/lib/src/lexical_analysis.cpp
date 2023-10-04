58975911aef51016b30d31e272ccdc702e71d8ba
EmilioRivera0 <elcholula619@gmail.com>
2023-10-04 10:22:20 -0600

    Subtokens search

--- src/lib/src/lexical_analysis.cpp
+++ src/lib/src/lexical_analysis.cpp
@@ -4,7 +4,11 @@
 void lexical_analysis(std::ifstream& source_file){
   // local variables declaration
   char line_buffer[LINE_BUFFER_SIZE]{""};
+  char token_buffer[TOKEN_BUFFER_SIZE]{""};
+  short int index{0};
   char* temp_c_ptr{nullptr};
+  char* token_it{nullptr};
+  std::vector<const char*> subtokens_vec{};
   
   // gel line
   while (source_file.getline(line_buffer, LINE_BUFFER_SIZE)) {
@@ -13,6 +17,16 @@ void lexical_analysis(std::ifstream& source_file){
 
     // get token
     while (temp_c_ptr != nullptr) {
+
+      // search for subtokens inside each token pointed by temp_c_ptr
+      subtokens_vec.push_back(temp_c_ptr);
+      token_it = temp_c_ptr;
+      while (*token_it != '\000') {
+        token_buffer[index] = *token_it;
+
+        token_it++;
+        index++;
+      }
 
       // lexical analyzer code ...
 
