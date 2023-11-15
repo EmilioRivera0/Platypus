6741a8d29332616749efa6ad9fe78d91fc6b0361
JuanPabloGHC <jpghc552@hotmail.com>
2023-11-15 10:02:33 -0600

    &

--- src/lib/headers/parser.h
+++ src/lib/headers/parser.h
@@ -12,7 +12,7 @@ class Parser
         std::map<std::string, std::string> &symbol_table;
         int tokens_index;
         int line_index;
-        std::map<unsigned, std::map<unsigned, std::string>> parser_errors;
+        std::map<unsigned, std::map<unsigned, std::string>> &parser_errors;
     public:
         Parser(std::map<int, std::vector<std::string>> &, std::map<std::string, std::string> &, std::map<unsigned, std::map<unsigned, std::string>> &);
         void analisis_parser();
